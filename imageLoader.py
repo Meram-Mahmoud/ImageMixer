2194a9ac653933066da6a4c07f282ef910ed5492
nariman-ahmed <nariman.sayed2003@gmail.com>
2024-12-08 14:26:45 +0200

    complete separation

--- imageLoader.py
+++ imageLoader.py
@@ -19,6 +19,7 @@ class ImageUploader(QWidget):
         self.contrast = 1    # Starting contrast (no change)
         self.is_dragging = False  # To track mouse dragging
 
+        self.image=None
         self.image_ft=FourierTransformViewer()
         self.image_loader = ImageLoader()
 
@@ -27,15 +28,6 @@ class ImageUploader(QWidget):
 
         layout.addWidget(self.image_loader.image_label)
 
-        # # Original Image Section
-        # self.original_image_label = QLabel(self)
-        # self.original_image_label.setAlignment(Qt.AlignCenter)
-        # self.original_image_label.setStyleSheet("border-radius: 10px; border: 2px solid #01240e;")
-        # self.original_image_label.setFixedSize(self.width, self.height)  # Rectangle size
-        # pixmap = QPixmap("ImageMixer/icons/coloredUpload.png")
-        # self.original_image_label.setPixmap(pixmap.scaled(64, 64, Qt.KeepAspectRatio))  # Adjust size as needed
-        # layout.addWidget(self.original_image_label)
-
         layout.addSpacing(20)  # Space between original image and FT section
 
 
@@ -43,47 +35,13 @@ class ImageUploader(QWidget):
         ft_layout.addWidget(self.image_ft.component_combo)  # Add FourierTransformViewer ComboBox
         ft_layout.addWidget(self.image_ft.ft_image_label)  # Add FourierTransformViewer FT Image
         layout.addLayout(ft_layout)
-
-        # # Fourier Transform Section (ComboBox + Rectangle)
-        # ft_layout = QVBoxLayout()  # Vertical layout for ComboBox and FT image
-
-        # # ComboBox
-        # self.component_combo = QComboBox(self)
-        # self.component_combo.addItem("Choose Component")
-        # self.component_combo.addItem("Magnitude")
-        # self.component_combo.addItem("Phase")
-        # self.component_combo.addItem("Real")
-        # self.component_combo.addItem("Imaginary")
-        # self.component_combo.setStyleSheet("""
-        #     QComboBox {
-        #         background-color: #01240e;
-        #         color: #ffffff;
-        #         border: 1px solid #888888;
-        #         border-radius: 5px;
-        #         padding: 5px;
-        #     }
-        #     QComboBox QAbstractItemView {
-        #         background: #ffffff;
-        #         selection-background-color: #01240e;
-        #         selection-color: #ffffff;
-        #         border: 1px solid #888888;
-        #     }
-        # """)
-        # self.component_combo.currentIndexChanged.connect(self.plot_fourier)
-        # ft_layout.addWidget(self.component_combo)  # Add ComboBox to the vertical layout
-
-        # # FT Image Rectangle
-        # self.ft_image_label = QLabel(self)
-        # self.ft_image_label.setAlignment(Qt.AlignCenter)
-        # self.ft_image_label.setStyleSheet("border-radius: 10px; border: 2px solid #01240e;")
-        # self.ft_image_label.setFixedSize(self.width, self.height)  # Same size as the original image label
-        # ft_layout.addWidget(self.ft_image_label)  # Add FT image to the vertical layout
-
-        # layout.addLayout(ft_layout)
     
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton and event.type() == QEvent.MouseButtonDblClick:
-            self.upload_image()
+            self.image=self.image_loader.upload_image()
+            self.image_ft.setData(self.image)
+            print("doneeeeee in the imageUploader class , MAGNITUDE HERE:")
+            print(self.image_ft.magnitude)
         elif event.button() == Qt.LeftButton:
             self.is_dragging = True
             self.last_pos = event.pos()  # Remember the mouse position
@@ -107,19 +65,6 @@ class ImageUploader(QWidget):
         if event.button() == Qt.LeftButton:
             self.is_dragging = False
 
-    # def update_image(self):
-    #     # Apply the current window/level (brightness/contrast) to the image
-    #     if self.image is not None:
-    #         adjusted_image = cv2.convertScaleAbs(self.image, alpha=self.contrast, beta=self.brightness)
-            
-    #         # Convert the adjusted image to QImage for display
-    #         height, width = adjusted_image.shape
-    #         qimage = QImage(adjusted_image.data, width, height, width, QImage.Format_Grayscale8)
-            
-    #         # Convert QImage to QPixmap and set it in the QLabel
-    #         pixmap = QPixmap.fromImage(qimage)
-    #         pixmap = self.get_rounded_pixmap(pixmap, self.original_image_label.size(), self.radius)
-    #         self.original_image_label.setPixmap(pixmap.scaled(self.width, self.height, Qt.KeepAspectRatio))
             
     def upload_image(self):
         file_path, _ = QFileDialog.getOpenFileName(self, "Select Image", "", "Images (*.png *.jpg *.bmp)")
@@ -143,32 +88,6 @@ class ImageUploader(QWidget):
             self.image_ft.setData(self.image)
             print("doneeeeee in the imageUploader class , MAGNITUDE HERE:")
             print(self.image_ft.magnitude)
-
-    
-    # Changing the style of the image to match the size of the rectangle
-    # def get_rounded_pixmap(self, pixmap, size, radius):
-    #     # Resize the pixmap to fit the QLabel
-    #     pixmap = pixmap.scaled(size, Qt.KeepAspectRatioByExpanding, Qt.SmoothTransformation)
-
-    #     # Create a transparent pixmap with the QLabel's size
-    #     rounded_pixmap = QPixmap(size)
-    #     rounded_pixmap.fill(Qt.transparent)
-
-    #     # Create a QPainter to draw the rounded pixmap
-    #     painter = QPainter(rounded_pixmap)
-    #     painter.setRenderHint(QPainter.Antialiasing)
-    #     painter.setRenderHint(QPainter.SmoothPixmapTransform)
-
-    #     # Create a rounded rectangle path
-    #     path = QPainterPath()
-    #     path.addRoundedRect(0, 0, size.width(), size.height(), radius, radius)
-
-    #     # Set the clip path and draw the pixmap
-    #     painter.setClipPath(path)
-    #     painter.drawPixmap(0, 0, pixmap)
-    #     painter.end()
-
-    #     return rounded_pixmap
 
 
 
